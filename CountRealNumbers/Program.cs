766780362d8354362b261826666293d74d44404b
Stranger <jmanova83@gmail.com>
2017-06-15 23:30:17 +0300

    01. Count Real Numbers/06 done

---
+++ CountRealNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountRealNumbers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var nums = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+			var dict = new SortedDictionary<double, int>();
+			foreach (var num in nums)
+			{
+				if (dict.ContainsKey(num))
+				{
+					dict[num]++;
+				}
+				else
+				{
+					dict[num] = 1;
+				}
+			}
+			foreach (var item in dict.Keys)
+			{
+				Console.WriteLine($"{item} -> {dict[item]}");
+			}
+		}
+	}
+}
