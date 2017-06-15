3234460a45ae7e187cb8897d299f4548e9817990
Stranger <jmanova83@gmail.com>
2017-06-15 22:56:27 +0300

    Odd occurrances solved

---
+++ OddOccurrances/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddOccurrances
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var words = Console.ReadLine().ToLower().Split(' ').ToArray();
+			var dict = new Dictionary<string, int>();
+			foreach (var word in words)
+			{
+				if (dict.ContainsKey(word))
+				{
+					dict[word]++;
+				}
+				else
+				{
+					dict[word] = 1;
+				}
+			}
+			var output = new List<string>();
+			foreach (var item in dict)
+			{
+				if (item.Value % 2 == 1)
+				{
+					output.Add(item.Key);
+				}
+			}
+			Console.WriteLine(string.Join(", ", output));
+		}
+	}
+}
